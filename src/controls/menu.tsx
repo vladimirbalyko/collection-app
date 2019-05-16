a66173e0ea6288001cc1bba2a60d9ef06b4741f8
Vladimir Balyko <vladimir.balyko@gmail.com>
2019-05-16 16:16:45 +0300

    Update menu link logic

--- src/controls/menu.tsx
+++ src/controls/menu.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import { classNames } from '.';
 
 enum Page {
     Homepage = 1,
@@ -15,33 +16,37 @@ export class Menu extends React.Component {
     onHomeClick = () => {
         this.setState({
             currentPage: Page.Homepage
-          });
+        });
     }
 
     onAboutClick = () => {
         this.setState({
             currentPage: Page.About
-          });
+        });
     }
 
     onContactClick = () => {
         this.setState({
             currentPage: Page.ContactUs
-          });
+        });
     }
+
+    isCurrentPage = (page: Page) => this.state.currentPage === page;
+
+    pageClassName = (page: Page) => classNames({ "current_page_item": this.isCurrentPage(page) });
 
     render() {
         return (
             <div id="menu-wrapper">
                 <div id="menu" className="container">
                     <ul>
-                        <li className="current_page_item">
+                        <li className={this.pageClassName(Page.Homepage)}>
                             <Link to="/" onClick={this.onHomeClick}>Homepage</Link>
                         </li>
-                        <li>
+                        <li className={this.pageClassName(Page.About)}>
                             <Link to="/about" onClick={this.onAboutClick}>About</Link>
                         </li>
-                        <li>
+                        <li className={this.pageClassName(Page.ContactUs)}>
                             <Link to="/contact" onClick={this.onContactClick}>Contact Us</Link>
                         </li>
                     </ul>
